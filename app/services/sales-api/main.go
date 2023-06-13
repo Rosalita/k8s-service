dc34b1a670910f64d3e0ada3c8f325196f60a384
Rosie Hamilton <enchantrix@gmail.com>
2023-06-13 15:00:08 +0100

    add GOMAXPROCS info and block until shutdown signal

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
 
 	"github.com/Rosalita/kind/foundation/logger"
 	"go.uber.org/zap"
@@ -24,5 +27,21 @@ func main() {
 }
 
 func run(log *zap.SugaredLogger) error {
+	// =========================================================================
+	// GOMAXPROCS
+	//
+	// GOMAXPROCS gets or sets the number of operating system threads the go program will use
+	// to execute Go routines. Passing 0 reports the maximum number of CPUs that can be 
+	// executing simultaneously. CPU is at 100% capacity when it is runnning the 
+	// GOMAXPROCS number of Go routines in parallel. The number of logical processors and cores
+	// should always be the same to avoid cycles being wasted by context switching.
+
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+	defer log.Infow("shutdown")
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown // blocks until able to read from shutdown.
+
 	return nil
 }
